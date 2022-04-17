c9f79a5b5d6ac5d295f7461cb595748789ec8812
warrior-coder <gudkou2003@gmail.com>
2022-04-17 13:02:20 +0300

    Add shaders files.

--- shaders/Sources/Main.c
+++ shaders/Sources/Main.c
@@ -1,21 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+
 #include <GLEW/glew.h>
 #include <GLFW/glfw3.h>
 
 /*
-GLEW (OpenGL Extension Wrangler) provides modern OpenGL functionality (shaders, vertex objects)
-GLFW (OpenGL FrameWork) provides creation and management multi-platform windows with OpenGL context and gives access to input 
+Shader Ц программа, котора€ выполн€етс€ на видеокарте
+Vertex Shader выполн€етс€ дл€ каждой вершины примитива
+Fragment Shader выполн€етс€ дл€ каждого пиксел€ примитива
+.glsl Ц OpenGL Shading Language (€зык программировани€ шейдеров)
  */
+
+ // загрузка кода шейдера в —-строку из файла
+char* LoadShader(const char* fileName)
+{
+    FILE* filePtr;
+
+    // открытие файла
+    fopen_s(&filePtr, fileName, "rb");
+    if (!filePtr)
+    {
+        printf("FILE_OPEN_ERROR: %s\n", fileName);
+        return NULL;
+    }
+
+    // вычисление размера файла
+    fseek(filePtr, 0, SEEK_END);
+    int fileSize = ftell(filePtr);
+    fseek(filePtr, 0, SEEK_SET);
+    printf("File size: %d\n", fileSize);
+
+    // чтение кода из файла
+	char* shaderCode = malloc((fileSize + 1) * sizeof(char));
+    if (!shaderCode)
+    {
+        printf("MEMORY_ALLOCATE_ERROR\n");
+        return NULL;
+    }
+    memset(shaderCode, '\0', fileSize + 1);
+    fread(shaderCode, sizeof(char), fileSize, filePtr);
+
+	printf("Code size: %d\n", strlen(shaderCode));
+	printf("Code:\n%s\n", shaderCode);
+
+    return shaderCode;
+}
 
 int main(void)
 {
-    // initialize GLFW
+    // инициализаци€ GLFW
     if (!glfwInit())
     {
         return 1;
     }
 
-    // create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(400, 400, "GLEW Start Window", NULL, NULL);
+    // создание окна с контекстом OpenGL
+    GLFWwindow* window = glfwCreateWindow(400, 400, "GLEW Shaders", NULL, NULL);
 
     if (!window)
     {
@@ -23,20 +65,95 @@ int main(void)
         return 2;
     }
 
-    // make the window's context current
+    // делаем контекст окна текущим контекстом отрисовки
     glfwMakeContextCurrent(window);
 
-    // initialize GLEW only after we created a valid OpenGL rendering context
+    // инициализаци€ GLEW (только после подключени€ контекста отрисовки OpenGL)
     if (glewInit() != GLEW_OK)
     {
         glfwTerminate();
         return 3;
     }
 
-    // loop until the user closes the window
+
+    GLint success;
+    GLchar infoLog[512];
+
+    // компил€ци€ вершинного шейдера
+    char* vertexShaderCode = LoadShader("Resources/VertexShader.glsl");
+    GLuint vertexShaderId = glCreateShader(GL_VERTEX_SHADER); // создает Vertex Shader и возвращает его id
+    glShaderSource(vertexShaderId, 1, &vertexShaderCode, NULL); // задаем текст кода шейдера
+    glCompileShader(vertexShaderId);
+    free(vertexShaderCode);
+
+    glGetShaderiv(vertexShaderId, GL_COMPILE_STATUS, &success); // проверка статуса компил€ции шейдера
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShaderId, 512, NULL, infoLog); // получаем текст ошибки
+        printf("VERTEX_SHADER_COMPILATION_FAILED: %s\n", infoLog);
+
+        // освобождение выделенной пам€ти
+        glDeleteShader(vertexShaderId);
+        glfwTerminate();
+
+        return 4;
+    }
+
+
+	// компил€ци€ фрагментного шейдера
+    char* fragmentShaderCode = LoadShader("Resources/FragmentShader.glsl");
+    GLuint fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER); // создает Fragment Shader и возвращает его id
+    glShaderSource(fragmentShaderId, 1, &fragmentShaderCode, NULL); // задаем текст кода шейдера
+    glCompileShader(fragmentShaderId);
+    free(fragmentShaderCode);
+
+    glGetShaderiv(fragmentShaderId, GL_COMPILE_STATUS, &success); // проверка статуса компил€ции шейдера
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShaderId, 512, NULL, infoLog); // получаем текст ошибки
+        printf("FRAGMENT_SHADER_COMPILATION_FAILED: %s\n", infoLog);
+
+        // освобождение выделенной пам€ти
+        glDeleteShader(fragmentShaderId);
+        glDeleteShader(vertexShaderId);
+        glfwTerminate();
+
+        return 5;
+    }
+
+
+    // создаем и линкуем шейдерную программу
+    GLuint programId = glCreateProgram(); // создаем шейдерную программу
+
+    glAttachShader(programId, fragmentShaderId); // присоедин€ем фрагментный шейдер к программе
+    glAttachShader(programId, vertexShaderId); // присоедин€ем вершинный шейдер к программе
+
+    glLinkProgram(programId); // св€зываем все шейдеры в программу
+
+    // удаление использованных шейдеров
+    glDeleteShader(fragmentShaderId);
+    glDeleteShader(vertexShaderId);
+
+    glGetProgramiv(programId, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShaderId, 512, NULL, infoLog);
+        printf("SHADER_LINK_FAILED: %s\n", infoLog);
+
+        // освобождение выделенной пам€ти
+        glDeleteProgram(programId);
+        glfwTerminate();
+
+        return 6;
+    }
+
+    glUseProgram(programId); // активируем программу отрисовки
+
+
+    // основной цикл программы
     while (!glfwWindowShouldClose(window))
     {
-        // render image
+		// отрисовка изображени€
         glClearColor(0.5f, 0.5f, 0.5f, 1);
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -50,14 +167,15 @@ int main(void)
 
         glPopMatrix();
 
-        // swap front and back buffers
+        // обмен переднего и заднего буферов
         glfwSwapBuffers(window);
 
-        // poll for and process events
+        // запрос на обработку событий
         glfwPollEvents();
     }
 
-    // free memory
+    // освобождение пам€ти
+    glDeleteProgram(programId);
     glfwTerminate();
 
     return 0;
